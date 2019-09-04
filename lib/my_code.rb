91779bfb64eee68a5b3fcc41b159dade36041438
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 12:58:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,15 @@ def map(array)
   new_array
 end
 
-def reduce()
+def reduce(array, starting_point=0)
+  counter = 0
+  total = starting_point
+ while counter < array.length
+ new_array.push(yield(array[counter]))
+ counter += 1 
+ end
+ total
+end
 
 
 
