d3d0c8888f75e3d36059f7bcf8b035abc50ade1c
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 12:07:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# Your Code Here
+def map(source_array, block)
+  counter = 0 
+  new_array = []
+  while counter < source_array.length do
+    new_array<<source_array[counter]{}
+    counter += 1 
+  end
+  yield(block)
+  new_array
+end
+
+ # map(source_array){|n| n*-1}
+  map(source_array)
+ # map(source_array)*2
+
+  
+ 
+  
+ 
+
+
+
+
+
+
+
+#def map_to_negativize(source_array, operation)
+#map_block.map{i*-1}
+#end
+
+#def map_to_no change(source_array)
+  
+#def map_to_double(source_array)
+  
+#def map_to_square(source_array)
+  
+
