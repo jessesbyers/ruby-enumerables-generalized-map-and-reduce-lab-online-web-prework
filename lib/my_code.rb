f2f88de540b22e2fd9b0057951234d6c3abeb376
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 13:14:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(array, starting_point=0)
   counter = 0
   total = starting_point
  while counter < array.length
- total += array[counter]
+ yield(total += array[counter])
  counter += 1
  end
  total
